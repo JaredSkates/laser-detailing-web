cd5c75c6d8c164ab4b69fa2948b606debc42a25e
JaredSkates <jaredskatesz12@gmail.com>
2024-10-11 21:43:14 -0700

    Added an image slider for about section

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,28 @@
-const images = document.getElementById("about-pictures");
+// Image Slider -> About Section
 let currentIndex = 0;
+const images = [
+    "assets/IMG_0239.jpg",
+    "assets/IMG_0246.jpg",
+    "assets/IMG_0250.jpg"
+];
+
+const sliderImage = document.getElementById('sliding-images');
+const leftArrow = document.getElementById('left-arrow');
+const rightArrow = document.getElementById('right-arrow');
+
+// Function to change image
+function changeImage(index) {
+    sliderImage.src = images[index]; // Changes the src directory
+}
+
+// Left arrow event listener (previous image)
+leftArrow.addEventListener('click', () => {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
+    changeImage(currentIndex);
+});
+
+// Right arrow event listener (next image)
+rightArrow.addEventListener('click', () => {
+    currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
+    changeImage(currentIndex);
+});
